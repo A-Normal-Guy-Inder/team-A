a1034dc27eadd01be3421abdc08a265a0aa17d55
A-Normal-Guy-Inder <isb.inder59433@gmail.com>
2026-08-03 20:37:41 +0530

    Changes before deployment

--- webapp-angular/src/environments/environment.production.ts
+++ webapp-angular/src/environments/environment.production.ts
@@ -1,8 +1,8 @@
 /* Production overrides */
 export const environment = {
   production: true,
-  apiUrl: '',
-  socketUrl: '',
+  apiUrl: 'https://hirehelper-backend.onrender.com/api',
+  socketUrl: 'https://hirehelper-backend.onrender.com',
   apiPort: '5000',
   pageSize: 12,
   apiTimeout: 90000,
